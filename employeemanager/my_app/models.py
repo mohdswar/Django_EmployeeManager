c314b190436ccb95428ccd8ad4dcc950c54e4312
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-22 22:31:49 +0300

    first model idid it

--- employeemanager/my_app/models.py
+++ employeemanager/my_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    
+    def __str__(self):
+        return self.name

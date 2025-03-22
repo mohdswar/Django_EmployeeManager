f406abf4681e58b84366043eee5192ea632cd261
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-22 21:31:59 +0300

    makeing some new temlpates

--- employeemanager/my_app/views.py
+++ employeemanager/my_app/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 def home(request):
     # Send a simple HTML response
     return HttpResponse('<h1>Hello world/h1>')
+    
 def about(request):
     return render(request, 'about.html')

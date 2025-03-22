8f0636422f2a0c5b5b1561c9653e2aebdd534e87
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-22 22:18:30 +0300

    renering daat in templates doneeeeeee!!!!!!

--- employeemanager/my_app/urls.py
+++ employeemanager/my_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views # Import views to connect routes to view functions
+
+from . import views 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('employees/', views.employee_index, name='employee-index'),
 ]
--- employeemanager/my_app/views.py
+++ employeemanager/my_app/views.py
@@ -4,7 +4,24 @@ from django.http import HttpResponse
 
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Hello world</h1>')
+        return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+class Employee:
+    def __init__(self, name, description, age):
+        self.name = name
+        self.description = description
+        self.age = age
+
+employees = [
+    Employee('Lolo',  'Kinda rude.', 3),
+    Employee('Sachi', 'Looks like a turtle.', 0),
+    Employee('Fancy', 'Happy fluff ball.', 4),
+    Employee('Bonk', 'Meows loudly.', 6)
+]
+
+def employee_index(request):
+    # Render the employee index template with the list of employees
+    return render(request, 'employees/index.html', {'employees': employees})

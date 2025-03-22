5fa3d64b70624371b92ede9256bba494d56674b2
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-22 21:26:38 +0300

    basic pages done

---
+++ employeemanager/my_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello world/h1>')
+def about(request):
+    return render(request, 'about.html')

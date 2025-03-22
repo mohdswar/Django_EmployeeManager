23c4b8dc75fb259b73cf29b4c979f745c0a26402
mohdswar9@gmail.com <mohdswar9@gmail.com>
2025-03-22 22:47:02 +0300

    its okay about orm in the website

--- employeemanager/my_app/urls.py
+++ employeemanager/my_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('employees/', views.employee_index, name='employee-index'),
-    path('employees/<int:employee_id>/', views.employee_detail, name='employee-detail'),
+     path('employees/<int:employee_id>/', views.employee_detail, name='employee-detail'),
 ]
--- employeemanager/my_app/views.py
+++ employeemanager/my_app/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Employee
 
 def home(request):
     # Send a simple HTML response
-        return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
-
 def employee_index(request):
-    employees=Employee,objects.all()
+    employees = Employee.objects.all()
     return render(request, 'employees/index.html', {'employees': employees})
 
 def employee_detail(request, employee_id):
-    employee=Employee.objects.get(id=employee_id)
+    employee = get_object_or_404(Employee, pk=employee_id)
     return render(request, 'employees/detail.html', {'employee': employee})
